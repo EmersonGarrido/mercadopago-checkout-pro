c6a9a6cb062e3e34b18318e0146cd2caac8e7653
EmersonGarrido <emersongarrido@hotmail.com.br>
2022-08-07 13:34:42 -0400

    chore:

---
+++ pages/payments/failure.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+// import { Container } from './styles';
+
+const Sucess: React.FC = () => {
+  return <div>deu ruim carai</div>;
+};
+
+export default Sucess;
---
+++ pages/payments/pending.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Sucess: React.FC = () => {
+  return <div>deu pendente carai</div>;
+};
+
+export default Sucess;
